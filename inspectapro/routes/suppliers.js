3ac8ee37436caaa6e34099cc0926e38d0bd6059f
Mateo654-source <escotiya102@gmail.com>
2026-03-02 22:01:07 -0500

    Delete inspectapro directory

--- inspectapro/routes/suppliers.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getSuppliers, updateSupplier } = require('../controllers/supplierController');
-const { authenticate, authorize } = require('../middlewares/auth');
-
-router.get('/', authenticate, getSuppliers);
-router.put('/:id', authenticate, authorize('ADMIN'), updateSupplier);
-
-module.exports = router;
